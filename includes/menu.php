5d4467a81c4c1e9cfcc248c790105ee69143ee1f
bruno.calazans <bruno.calazans@presidencia.gov.br>
2025-05-04 12:30:19 -0300

    teste

--- includes/menu.php
+++ includes/menu.php
@@ -7,19 +7,19 @@
         <title></title>
         <link rel="stylesheet" href="css/app-menu.css" type="text/css">
     </head>
-    <nav class="navbar navbar-expand-lg bg-body -tertiary" style="background-color: #E10600">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto">
-                <li>
-                    <a href="?secao=home">
-                        <img src="images/f1_logo.png" alt="">
-                    </a>
-                </li>
-                <li class="nav-item me-3"><a href="?secao=home" class="text-white nav-link">Home</a></li>
-                <li class="nav-item me-3"><a href="?secao=equipes" class="text-white nav-link">Equipes</a></li>
-                <li class="nav-item me-3"><a href="?secao=pilotos" class="text-white nav-link">Pilotos</a></li>
-                <li class="nav-item me-3"><a href="?secao=corrida" class="text-white nav-link">Corrida</a></li>
-            </ul>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #E10600;">
+        <div class="d-flex align-items-center">
+            <a href="?secao=home" class="me-4">
+                <img src="images/f1_logo.png">
+            </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item me-3"><a href="?secao=home" class="text-white nav-link">Home</a></li>
+                    <li class="nav-item me-3"><a href="?secao=equipes" class="text-white nav-link">Equipes</a></li>
+                    <li class="nav-item me-3"><a href="?secao=pilotos" class="text-white nav-link">Pilotos</a></li>
+                    <li class="nav-item me-3"><a href="?secao=corrida" class="text-white nav-link">Corrida</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
 </html>
