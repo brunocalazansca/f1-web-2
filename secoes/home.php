c8425e5b9446ded07b7d7194d619f5b68194fc96
brunocalazansca <bcalazanscarritilha@gmail.com>
2025-05-15 11:12:06 -0300

    Centralizar imagens

--- secoes/home.php
+++ secoes/home.php
@@ -8,9 +8,9 @@ $noticias = $noticiaRecebida->getAllDataTable();
     foreach ($noticias as $noticia) {
 ?>
 
-    <div class="card mb-3">
-        <img src='<?= $noticia["url"] ?>'>
-        <div class="card-body">
+    <div class="card mb-3 w-50 mx-auto">
+        <img class="img-fluid" src='<?= $noticia["url"] ?>'>
+        <div class="card-body mx-auto">
             <h5 class="card-title"><?= $noticia["titulo"] ?></h5>
             <p class="card-text"><?= $noticia["descricao"] ?></p>
             <p class="card-text"><small class="text-body-secondary"><?= $noticia["data"] ?> - <?= $noticia["autor"] ?></small></p>
