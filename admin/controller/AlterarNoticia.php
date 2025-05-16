5d21c34cecb007cac0fba7b81c77b8e8d0f79fe7
bruno.calazans <bruno.calazans@presidencia.gov.br>
2025-05-15 21:43:55 -0300

    Implementar alteração das notícias

--- admin/controller/AlterarNoticia.php
+++ admin/controller/AlterarNoticia.php
@@ -3,27 +3,32 @@
     include_once ("../../model/Noticia.php");
     include_once ("../../classes/manipulaDados.php");
 
+    function convert($String) {
+        return iconv ("UTF-8", "ISO8859-1", $String);
+    }
+
     $id = $_POST['id'];
     $titulo = $_POST['titulo'];
     $descricao = $_POST['descricao'];
     $autor = $_POST['autor'];
     $imagem = $_FILES['arquivo'];
 
-    $urlImagem = null;
+    $urlBanco = null;
 
-    if ($imagem['error'] === UPLOAD_ERR_OK) {
-        $pastaDestino = '../../imgNoticias/';
-        $nomeArquivo = uniqid() . "_" . basename($imagem['name']);
-        $caminhoCompleto = $pastaDestino . $nomeArquivo;
+    if (!empty($_FILES['arquivo']['name'])) {
+        $nomeArquivoSalvo = uniqid() . "_" . convert($_FILES['arquivo']['name']);
+        $urlBanco = "imgNoticias/" . $nomeArquivoSalvo;
+        $urlLocalSalvo = '../../imgNoticias/' . $nomeArquivoSalvo;
 
-        if (move_uploaded_file($imagem['tmp_name'], $caminhoCompleto)) {
-            $urlImagem = 'imgNoticias/' . $nomeArquivo;
+        if (!move_uploaded_file($_FILES['arquivo']['tmp_name'], $urlLocalSalvo)) {
+            echo "Erro ao mover o arquivo!";
+            exit;
         }
     }
 
-    $camposUpdate = "titulo = '$titulo', descricao = '$descricao', autor = '$autor'";
-    if ($urlImagem !== null) {
-        $camposUpdate .= ", url = '$urlImagem'";
+    $camposUpdate = "titulo = '{$titulo}', descricao = '{$descricao}', autor = '{$autor}'";
+    if ($urlBanco !== null) {
+        $camposUpdate .= ", url = '{$urlBanco}'";
     }
 
     $conexao = new ManipulaDados();
